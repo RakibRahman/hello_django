b808c34ff675162f3a12b02f84cded0849c2423e
rakibrahman <md.rr.talukder@gmail.com>
2023-12-09 22:17:02 +0600

    class-9 assginment

--- hello_django/courses/views.py
+++ hello_django/courses/views.py
@@ -4,8 +4,37 @@ from django.http import HttpResponse
 # Create your views here.
 
 def course(req):
-    print('req',req)
-    return render(req,'home.html')
+    coursesList =[
+    {
+        "course_id": 1,
+        "course_name": "Introduction to Python",
+        "instructor": "John Smith",
+        "duration": 6,
+        "image_url": "https://picsum.photos/200/300?random=2",
+    },
+    {
+        "course_id": 2,
+        "course_name": "Data Science Fundamentals",
+        "instructor": "Jane Doe",
+        "duration": 8,
+        "image_url": "https://picsum.photos/200/300?random=12",
+    },
+    {
+        "course_id": 3,
+        "course_name": "Web Development with Django",
+        "instructor": "Bob Johnson",
+        "duration": 10,
+        "image_url": "https://picsum.photos/200/300?random=20",
+    },
+    {
+        "course_id": 4,
+        "course_name": "Machine Learning Basics",
+        "instructor": "Alice Williams",
+        "duration": 7,
+        "image_url": "https://picsum.photos/200/300?random=21",
+    },
+]
+    return render(req,'home.html',context={'coursesList':coursesList})
 
 def profile(req):
     print('req',req)
